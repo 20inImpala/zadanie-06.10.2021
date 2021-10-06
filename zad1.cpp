1c371ac1ed88d6a0c29c0830a0d46c671ad0ce06
20inImpala <92028683+20inImpala@users.noreply.github.com>
2021-10-06 14:06:20 +0200

    Add files via upload

---
+++ zad1.cpp
@@ -0,0 +1,25 @@
+public class Triangle{
+	public int bok_a; 
+	
+	public int bok_b;
+	
+	public int bok_c;
+		
+	public int obliczPole(int a, int b, int c){
+		return a * b * c;
+	}
+	public int obliczObwod(int a, int b, int c){
+		return = a + b + c;
+	}
+}
+Triangle trojkat;
+
+trojkat.bok_a = 1;
+
+trojkat.bok_b = 2;
+
+trojkat.bok_c = 3;
+
+int pole = obliczPole(trojkat.bok_a, trojkat.bok_b, trojkat.bok_c);
+
+int obwod = obliczObwod(trojkat.bok_a, trojkat.bok_b, trojkat.bok_c);
